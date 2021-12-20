4f2ab03c3b0f4fd2c9d15734fe62168eb4661395
apieceofcode1801 <apieceofcode1801@gmail.com>
2021-12-20 11:26:52 +0700

    add button swizzling

--- Swizzling_Sample/ViewController.swift
+++ Swizzling_Sample/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
+        UIButton.xxx_swizzleSendAction()
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func tapOnButton() {
+        print("Button tapped")
+    }
 }
 
+extension UIButton {
+    class func xxx_swizzleSendAction() {
+        struct xxx_swizzleToken {
+            static var onceToken : Int = 0
+        }
+        
+        let cls: AnyClass! = UIButton.self
+        
+        let originalSelector = #selector(sendAction(_:to:for:))
+        let swizzledSelector = #selector(xxx_sendAction(_:to:for:))
+        
+        guard let originalMethod =
+                class_getInstanceMethod(cls, originalSelector), let swizzledMethod =
+                class_getInstanceMethod(cls, swizzledSelector) else { return }
+        
+        method_exchangeImplementations(originalMethod, swizzledMethod)
+    }
+    @objc public func xxx_sendAction(_ action: Selector, to target: Any?, for event: UIEvent?)
+    {
+        struct xxx_buttonTapCounter {
+            static var count: Int = 0
+        }
+        
+        xxx_buttonTapCounter.count += 1
+        print(xxx_buttonTapCounter.count)
+        xxx_sendAction(action, to: target, for: event)
+    }
+}
